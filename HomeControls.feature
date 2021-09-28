9eab26af1de96a92cd81d2578e084e3443eae8d6
Ashutosh Samantasinghar <64477215+ashutoshsipu@users.noreply.github.com>
2021-09-28 17:15:30 +0530

    Add files via upload

---
+++ HomeControls.feature
@@ -0,0 +1,9 @@
+﻿Feature: HomeControls
+	Simple logins in HomeControls website
+
+@scopeBinding
+Scenario: HomeControls should be login with given username and Password
+	Given I have navigated to HomeControls website
+	And I have entered username and password
+	When I click on submit button
+	Then I should see the homepage
---
+++ HomeControlsSteps.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumSpecflowStarter.StepBindings
+{
+    [Binding]
+    public class HomeControlsSteps
+    {
+        private String submitKey;
+        private ChromeDriver chromeDriver;
+
+        public HomeControlsSteps() => chromeDriver = new ChromeDriver();
+        [Given(@"I have navigated to HomeControls website")]
+        public void GivenIHaveNavigatedToHomeControlsWebsite()
+        {
+            chromeDriver.Navigate().GoToUrl("https://www.homecontrols.com/");
+            chromeDriver.Manage().Window.Maximize();
+            //Assert.IsTrue(chromeDriver.Title.ToLower().Contains("Home Controls Incorporate"));
+
+        }
+        
+        [Given(@"I have entered username and password")]
+        public void GivenIHaveEnteredUsernameAndPassword()
+        {
+            chromeDriver.FindElement(By.XPath("/html/body/div[3]/div/div/header/div[3]/nav/div[3]/div[2]/div[2]/div/ul/li[1]/a")).Click();
+            chromeDriver.FindElement(By.XPath("/html/body/div[4]/div/div/div/div[3]/div/section/div/div[1]/div/form/fieldset/div[1]/div/input")).Click();
+            chromeDriver.FindElement(By.XPath("/html/body/div[4]/div/div/div/div[3]/div/section/div/div[1]/div/form/fieldset/div[1]/div/input")).SendKeys("ashutoshsamantasinghar1@gmail.com");
+            chromeDriver.FindElement(By.XPath("/html/body/div[4]/div/div/div/div[3]/div/section/div/div[1]/div/form/fieldset/div[2]/div/input")).Click();
+            chromeDriver.FindElement(By.XPath("/html/body/div[4]/div/div/div/div[3]/div/section/div/div[1]/div/form/fieldset/div[2]/div/input")).SendKeys("ashutosh");
+        }
+        
+        [When(@"I click on submit button")]
+        public void WhenIClickOnSubmitButton()
+        {
+            chromeDriver.FindElement(By.XPath("/html/body/div[4]/div/div/div/div[3]/div/section/div/div[1]/div/form/fieldset/div[6]/button")).Submit();
+        }
+        
+        [Then(@"I should see the homepage")]
+        public void ThenIShouldSeeTheHomepage()
+        {
+            Assert.IsTrue(chromeDriver.Title.ToLower().Contains("home controls - smart home automation leader"));
+        }
+
+        public void Dispose()
+        {
+            if(chromeDriver !=null)
+            {
+                chromeDriver.Dispose();
+                chromeDriver = null;
+            }
+
+        }
+
+    }
+}
